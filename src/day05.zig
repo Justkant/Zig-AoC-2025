8f38a2cc97c6b5e1abda1e1410f250a140ff9af2
Kant <quent92100@gmail.com>
2025-12-05 14:50:25 +0100

    feat: day05

--- src/day05.zig
+++ src/day05.zig
@@ -33,14 +33,90 @@ const gpa = util.gpa;
 const data = @embedFile("data/day05.txt");
 const data_ex = @embedFile("data/day05_ex.txt");
 
+const Range = struct {
+    start: usize,
+    end: usize,
+
+    fn inRange(self: Range, id: usize) bool {
+        return self.start <= id and id <= self.end;
+    }
+
+    fn overlap(self: Range, range: Range) bool {
+        return self.start <= range.end and range.start <= self.end;
+    }
+};
+
 fn part1(buffer: []const u8) !usize {
-    _ = buffer; // autofix
-    return 0;
+    var lineIterator = tokenizeAny(u8, buffer, "\r\n");
+    var ranges = List(Range).empty;
+
+    var total: usize = 0;
+    while (lineIterator.next()) |line| {
+        var rangeIterator = tokenizeSca(u8, line, '-');
+
+        const rangeStart = rangeIterator.next();
+        const rangeEnd = rangeIterator.next();
+
+        if (rangeEnd == null) {
+            const id = try parseInt(usize, rangeStart.?, 10);
+            for (ranges.items) |range| {
+                if (range.inRange(id)) {
+                    total += 1;
+                    break;
+                }
+            }
+        } else if (rangeStart != null and rangeEnd != null) {
+            try ranges.append(gpa, .{
+                .start = try parseInt(usize, rangeStart.?, 10),
+                .end = try parseInt(usize, rangeEnd.?, 10),
+            });
+        }
+        // print("line: {s} | total: {d}\n", .{ line, total });
+    }
+
+    return total;
 }
 
 fn part2(buffer: []const u8) !usize {
-    _ = buffer; // autofix
-    return 0;
+    var lineIterator = tokenizeAny(u8, buffer, "\r\n");
+    var ranges = List(Range).empty;
+
+    while (lineIterator.next()) |line| {
+        var rangeIterator = tokenizeSca(u8, line, '-');
+
+        const rangeStart = rangeIterator.next();
+        const rangeEnd = rangeIterator.next();
+
+        if (rangeEnd == null) {
+            break;
+        } else if (rangeStart != null and rangeEnd != null) {
+            var range: Range = .{
+                .start = try parseInt(usize, rangeStart.?, 10),
+                .end = try parseInt(usize, rangeEnd.?, 10),
+            };
+
+            var i: usize = 0;
+            while (i < ranges.items.len) {
+                if (range.overlap(ranges.items[i])) {
+                    range.start = @min(range.start, ranges.items[i].start);
+                    range.end = @max(range.end, ranges.items[i].end);
+                    _ = ranges.swapRemove(i);
+                    continue;
+                }
+                i += 1;
+            }
+            try ranges.append(gpa, range);
+        }
+        // print("line: {s}\n", .{line});
+    }
+
+    var total: usize = 0;
+    for (ranges.items) |range| {
+        total += range.end + 1 - range.start;
+        // print("range: {} | diff: {d} | total {d}\n", .{ range, range.end + 1 - range.start, total });
+    }
+
+    return total;
 }
 
 pub fn main() !void {
@@ -52,13 +128,13 @@ pub fn main() !void {
 }
 
 test "part1" {
-    const expected = 0;
+    const expected = 3;
     const actual = try part1(data_ex);
     try std.testing.expectEqual(expected, actual);
 }
 
 test "part2" {
-    const expected = 0;
+    const expected = 14;
     const actual = try part2(data_ex);
     try std.testing.expectEqual(expected, actual);
 }
