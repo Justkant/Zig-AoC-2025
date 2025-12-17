19116941c884f83d7935e29685007cb26a707833
Kant <quent92100@gmail.com>
2025-12-17 21:34:32 +0100

    feat: day09

--- src/day09.zig
+++ src/day09.zig
@@ -1,6 +1,7 @@
 const std = @import("std");
 const Allocator = std.mem.Allocator;
 const List = std.ArrayList;
+const PriorityQueue = std.PriorityQueue;
 const Map = std.AutoHashMap;
 const StrMap = std.StringHashMap;
 const BitSet = std.DynamicBitSet;
@@ -33,13 +34,141 @@ const gpa = util.gpa;
 const data = @embedFile("data/day09.txt");
 const data_ex = @embedFile("data/day09_ex.txt");
 
+fn absDiff(a: usize, b: usize) usize {
+    return if (a > b) a - b else b - a;
+}
+
+const Pos = struct {
+    x: usize,
+    y: usize,
+    fn square(self: Pos, pos: Pos) usize {
+        return (absDiff(self.x, pos.x) + 1) * (absDiff(self.y, pos.y) + 1);
+    }
+    fn eql(self: Pos, pos: Pos) bool {
+        return self.x == pos.x and self.y == pos.y;
+    }
+};
+
+const Square = struct {
+    firstCorner: Pos,
+    secondCorner: Pos,
+    area: usize,
+};
+
+fn descArea(context: void, a: Square, b: Square) std.math.Order {
+    _ = context;
+    return std.math.order(b.area, a.area);
+}
+
+const SquaresPQ = PriorityQueue(
+    Square,
+    void,
+    descArea,
+);
+
+fn getSquares(squares: *SquaresPQ, tiles: []Pos) !void {
+    for (0..tiles.len) |i| {
+        for (i + 1..tiles.len) |j| {
+            const area = tiles[i].square(tiles[j]);
+            try squares.add(.{
+                .firstCorner = tiles[i],
+                .secondCorner = tiles[j],
+                .area = area,
+            });
+        }
+    }
+}
+
 fn part1(buffer: []const u8) !usize {
-    _ = buffer; // autofix
-    return 0;
+    const lineSep = "\r\n";
+    var lineIterator = tokenizeAny(u8, buffer, lineSep);
+    var tiles = List(Pos).empty;
+    defer tiles.deinit(gpa);
+
+    while (lineIterator.next()) |line| {
+        var posIterator = splitSca(u8, line, ',');
+        try tiles.append(gpa, .{
+            .x = try parseInt(usize, posIterator.next().?, 10),
+            .y = try parseInt(usize, posIterator.next().?, 10),
+        });
+    }
+
+    var squares = SquaresPQ.init(gpa, {});
+    defer squares.deinit();
+    try getSquares(&squares, tiles.items);
+
+    // print("tiles: {any}\n", .{tiles.items});
+    const biggestSquare = squares.remove();
+    // print("biggest square: {any}\n", .{biggestSquare});
+
+    return biggestSquare.area;
+}
+
+fn isSquareInTiles(square: Square, tiles: List(Pos)) bool {
+    const leftCol = @min(square.firstCorner.x, square.secondCorner.x);
+    const rightCol = @max(square.firstCorner.x, square.secondCorner.x);
+    const topRow = @min(square.firstCorner.y, square.secondCorner.y);
+    const bottomRow = @max(square.firstCorner.y, square.secondCorner.y);
+
+    var i: usize = 0;
+    var j: usize = tiles.items.len - 1;
+    while (i < tiles.items.len) : ({
+        j = i;
+        i += 1;
+    }) {
+        const a = tiles.items[i];
+        const b = tiles.items[j];
+
+        if (a.x == b.x) {
+            // vertical edge
+            const col = a.x;
+            // check vertical edge is within the cols of the current rectangle
+            if (col <= leftCol or col >= rightCol) continue;
+
+            const topEdge = @min(a.y, b.y);
+            const bottomEdge = @max(a.y, b.y);
+            if (topEdge <= topRow and bottomEdge > topRow) return false;
+            if (topEdge < bottomRow and bottomEdge >= bottomRow) return false;
+        } else {
+            // horizontal edge
+            const row = a.y;
+            // check horizontal edge is within the rows of the current rectangle
+            if (row <= topRow or row >= bottomRow) continue;
+
+            const leftEdge = @min(a.x, b.x);
+            const rightEdge = @max(a.x, b.x);
+            if (leftEdge <= leftCol and rightEdge > leftCol) return false;
+            if (leftEdge < rightCol and rightEdge >= rightCol) return false;
+        }
+    }
+    return true;
 }
 
 fn part2(buffer: []const u8) !usize {
-    _ = buffer; // autofix
+    const lineSep = "\r\n";
+    var lineIterator = tokenizeAny(u8, buffer, lineSep);
+    var tiles = List(Pos).empty;
+    defer tiles.deinit(gpa);
+
+    while (lineIterator.next()) |line| {
+        var posIterator = splitSca(u8, line, ',');
+        try tiles.append(gpa, .{
+            .x = try parseInt(usize, posIterator.next().?, 10),
+            .y = try parseInt(usize, posIterator.next().?, 10),
+        });
+    }
+
+    var squares = SquaresPQ.init(gpa, {});
+    defer squares.deinit();
+    try getSquares(&squares, tiles.items);
+
+    for (0..squares.items.len) |_| {
+        const square = squares.remove();
+        if (isSquareInTiles(square, tiles)) {
+            return square.area;
+        }
+    }
+
     return 0;
 }
 
@@ -52,13 +181,13 @@ pub fn main() !void {
 }
 
 test "part1" {
-    const expected = 0;
+    const expected = 50;
     const actual = try part1(data_ex);
     try std.testing.expectEqual(expected, actual);
 }
 
 test "part2" {
-    const expected = 0;
+    const expected = 24;
     const actual = try part2(data_ex);
     try std.testing.expectEqual(expected, actual);
 }
